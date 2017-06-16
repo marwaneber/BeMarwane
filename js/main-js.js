6b3093f653c3b0259c2893886bf165c647a7e289
Marwane Beriich <mberiich@localhost.localdomain>
2017-06-16 17:31:03 +0000

    Marwane Beriich update : split style file into 3 files (style.css ,responsive.css and animations.css) also added the logo and shurtcut icon (logo.svg and favicon.png)into index.html added the loding page and js file (main-js.js)

---
+++ js/main-js.js
@@ -0,0 +1,14 @@
+
+ // Loading function
+document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'interactive') {
+       document.getElementById('contents').style.visibility="hidden";
+  } else if (state == 'complete') {
+      setTimeout(function(){
+         document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+         document.getElementById('contents').style.visibility="visible";
+     },1500);
+  }
+}
