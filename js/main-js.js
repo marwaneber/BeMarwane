735c1176b35df1ab6728863195c555d9803a1b48
Marwane Beriich <mberiich@localhost.localdomain>
2017-06-16 18:26:11 +0000

    Marwane Beriich update : activingand styling the loading page

--- js/main-js.js
+++ js/main-js.js
@@ -3,12 +3,12 @@
 document.onreadystatechange = function () {
   var state = document.readyState
   if (state == 'interactive') {
-       document.getElementById('contents').style.visibility="hidden";
+       document.getElementById('contents').style.display="none";
   } else if (state == 'complete') {
       setTimeout(function(){
          document.getElementById('interactive');
-         document.getElementById('load').style.visibility="hidden";
-         document.getElementById('contents').style.visibility="visible";
+         document.getElementById('load').style.display="none";
+         document.getElementById('contents').style.display="block";
      },1500);
   }
 }
