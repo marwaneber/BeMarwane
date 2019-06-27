fe2ce0b742dff43d8f50636af7b94ae66a8cab72
Marwane Beriich <marber.mb11@gmail.com>
2019-06-27 01:37:04 +0100

    Adding vue to the Project

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <img alt="Vue logo" :src="require('@/assets/logo.png')">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  height: 40px;
+}
+
+#nav img {
+  height: 100%;
+  float: left;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #0080FF;
+  padding: 10px;
+  float: right;  text-decoration: none;
+}
+a:hover {
+
+      background: rgba(181, 177, 177, 0.2);
+}
+#nav a:hover {
+
+      background: rgba(181, 177, 177, 0.2);
+}
+
+#nav a.router-link-exact-active {
+  color: #444;
+  text-decoration: underline;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <div>
+      <div id="nav_follow">
+        <a href="https://www.instagram.com/marwaneberiich/">Instagram</a>
+        <a href="https://twitter.com/MarwaneBeriich/">Twitter</a>
+        <a href="https://www.facebook.com/MarwaneBeriich">FaceBook</a>
+        <a href="https://github.com/marwaneber/">GitHub</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+#nav_follow {
+  margin: 30px;
+  margin-top: 0;
+  color: blue;
+  border: 1px solid #0080FF;
+  overflow: auto;
+}
+
+#nav_follow a {
+  color: #0080FF;
+  float: left;
+  padding: 10px 0;
+  width: 25%;
+  box-sizing: border-box;
+  display: inline;
+  text-align: center;
+}
+</style>
+
